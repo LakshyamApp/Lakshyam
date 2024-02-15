f00ee2d4a5323f3a62437a54c30609bcdb375999
Vanka Siddhartha <97089805+vankaSiddhartha@users.noreply.github.com>
2024-02-15 23:52:09 +0530

    initial commit

---
+++ app/src/main/java/com/santhi/lakshyam/onbording/ui/StartupScreenAcitivity.kt
@@ -0,0 +1,27 @@
+package com.santhi.lakshyam.onbording.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.santhi.lakshyam.MainActivity
+import com.santhi.lakshyam.R
+
+class StartupScreenAcitivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_startup_screen_acitivity)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app's main activity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+            // Close this activity
+            finish()
+        }, 2000)
+    }
+}
